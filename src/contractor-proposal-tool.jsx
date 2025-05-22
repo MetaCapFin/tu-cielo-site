aede35b3404ebcf4bf35875f324e529e837f409e
MetaCapFin <advisor@metacap.finance>
2025-05-22 11:44:57 -0500

    Update and rename contractor-proposal-tool.js to contractor-proposal-tool.jsx

---
+++ src/contractor-proposal-tool.jsx
@@ -0,0 +1,13 @@
+// src/contractor-proposal-tool.jsx
+import React from 'react';
+
+export default function ContractorProposalTool() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Contractor Proposal Tool</h1>
+      <p>This feature is coming soon! Stay tuned.</p>
+      {/* You can add more mock UI here */}
+    </div>
+  );
+}
+
