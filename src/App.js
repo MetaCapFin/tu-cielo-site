15da39c295c6f481cf90b87e2aa5d2a9508607c6
MetaCapFin <advisor@metacap.finance>
2025-05-26 12:35:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -572,7 +572,7 @@ function Homepage() {
 
       {/* FOOTER */}
       <footer className="site-footer">
-        <p>© 2023 Tu Cielo. All rights reserved.</p>
+        <p>© 2025 Tu Cielo. All rights reserved.</p>
         <br></br>
         <p>
           Disclaimer: TuCielo and any affiliated entities do not provide installation services and are not government agencies. All terms, conditions,
